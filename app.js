aa415008131344785505b6721b1e98896f0a6124
Black Baba <amrinder.dhindsa1@gmail.com>
2019-10-20 16:27:58 +0530

    Create connection with MongoDB and setup views

--- app.js
+++ app.js
@@ -1,46 +1,29 @@
 var express = require("express");
-var morgan = require("morgan");
+var mongoose = require("mongoose");
 var path = require("path");
-var zipdb = require("zippity-do-dah");
-var ForecastIo = require("forecastio");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var flash = require("connect-flash");
 
+var routes = require("./routes");
 var app = express();
-app.use(morgan("short"));
+mongoose.connect("mongodb://localhost:27017/test");
 
-var weather = new ForecastIo("6b26bdc4e715ff93d9ace86dcc5b36cb");
-
-app.use(express.static(path.resolve(__dirname, "public")));
-
-app.set("views", path.resolve(__dirname, "views"));
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.get("/", function (req, res) {
-  res.render("index");
+app.use(session({
+  secret: "TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+app.use(routes);
+app.listen(app.get("port"), function () {
+  console.log("Server started on port " + app.get("port"));
 });
-
-app.get(/^\/(\d{5})$/, function (req, res, next) {
-  var zipcode = req.params[0];
-  var location = zipdb.zipcode(zipcode);
-  if (!location.zipcode) {
-    next();
-    return;
-  }
-  var latitude = location.latitude;
-  var longitude = location.longitude;
-  weather.forecast(latitude, longitude, function (err, data) {
-    if (err) {
-      next();
-      return;
-    }
-    res.json({
-      zipcode: zipcode,
-      temperature: data.currently.temperature
-    });
-  });
-});
-
-app.use(function (req, res) {
-  res.status(404).render("404");
-});
-
-app.listen(3000);
